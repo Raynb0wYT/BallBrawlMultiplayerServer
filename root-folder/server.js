16da3e2d838afdbaf53b60a08cdfcfe28408d28f
Liam Morlen <liam@morlen.org>
2025-05-27 22:44:27 -0500

    Update server.js

--- root-folder/server.js
+++ root-folder/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { 
-    origin: "*", 
+    origin: "https://127.0.0.1", 
     methods: ["GET", "POST"]
   }
 });
